d00c9d127b4943a1922802d9552c1a69f30c1c70
Richard Williams <richelliswilliams@gmail.com>
2020-12-23 17:00:42 -0800

    finish add shtufflist

--- main_app/forms.py
+++
@@ -1,8 +0,0 @@
-from django.forms import ModelForm
-from .models import ShtuffList
-
-class ShtuffListsForm(ModelForm):
-  class Meta:
-    model = ShtuffList
-    fields = ['name', 'description','image']
-
